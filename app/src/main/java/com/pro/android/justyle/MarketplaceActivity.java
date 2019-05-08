4089c8523c88b2a10634043d93c6613a49815a01
Yazz97 <yazz1y@live.dk>
2019-05-08 11:34:54 +0200

    Yazz er for sej til Ilayda

--- app/src/main/java/com/pro/android/justyle/MarketplaceActivity.java
+++ app/src/main/java/com/pro/android/justyle/MarketplaceActivity.java
@@ -7,13 +7,19 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
 // import android.widget.Toolbar;
 
 import com.google.android.gms.dynamic.OnDelegateCreatedListener;
@@ -21,6 +27,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
@@ -35,10 +42,16 @@ public class MarketplaceActivity extends AppCompatActivity implements ImageAdapt
     private RecyclerView mRecyclerView;
     private ImageAdapter mAdapter;
     private FirebaseStorage mStorage;
+    private DatabaseReference LikeRef;
     private DatabaseReference mDatabaseRef;
     private ValueEventListener mDBListener;
     private List<Upload> mUploads;
     MaterialSearchView searchView;
+    ImageButton LikepostButton, CommentPostButton;
+    TextView displayNoOfLikes;
+    EditText editText;
+    
+  //  Boolean LikeChecker = false;
 
 
 
@@ -47,28 +60,76 @@ public class MarketplaceActivity extends AppCompatActivity implements ImageAdapt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.marketplace);
 
+     //   LikepostButton = (ImageButton) mRecyclerView.findViewById(R.id.like_button);
+    //    CommentPostButton = (ImageButton) mRecyclerView.findViewById(R.id.commentButton);
+    //    displayNoOfLikes = (TextView) mRecyclerView.findViewById(R.id.display_no_of_likes);
+
+     //   LikepostButton.setOnClickListener(new View.OnClickListener() {
+       //     @Override
+         //   public void onClick(View v) {
+         //       LikeChecker = true;
+  /*              LikeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                    {
+                        if(dataSnapshot.child(PostKey).hasChild(currentUserID))
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+*/       // });
 
         mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        editText = (EditText)findViewById(R.id.searchView);
+
+      // Search bar, TextWatcher allow us to instantly update data on other views
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {    // The count characters beginning at start are about to be replaced
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {      //  The characters beginning at start have just replaced old text
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {                              // The text has been changed 
+                if (!s.toString().isEmpty()) {
+                    search(s.toString());
+
+
+                } else {
+                    search("");
+
+                }
+            } 
+            
+
+        });
+
 
         mUploads = new ArrayList<>();
-
-
         mAdapter = new ImageAdapter(MarketplaceActivity.this, mUploads);
         mRecyclerView.setAdapter(mAdapter);
         mAdapter.setOnItemClickListener(MarketplaceActivity.this);
-
-
         mStorage = FirebaseStorage.getInstance();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("marketplace");
-
+        LikeRef = FirebaseDatabase.getInstance().getReference().child("Likes");
         mDBListener = mDatabaseRef.addValueEventListener(new ValueEventListener() {
 
             {
 
             }
-
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -87,9 +148,40 @@ public class MarketplaceActivity extends AppCompatActivity implements ImageAdapt
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                Toast.makeText(MarketplaceActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MarketplaceActivity.this, databaseError.getMessage(),  Toast.LENGTH_SHORT).show();
             }
-        });
+        });                                                                
+    }
+// Search bar, query is linked with the database and 
+    private void search(String toString) {
+ Query query = mDatabaseRef.orderByChild("name")
+         .startAt(toString)
+         .endAt(toString+"\uf8ff");
+
+ query.addValueEventListener(new ValueEventListener() {
+     @Override
+     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+         if(dataSnapshot.hasChildren()) {
+             mUploads.clear();
+             for(DataSnapshot dss: dataSnapshot.getChildren())
+             {
+               final Upload upload = dss.getValue(Upload.class);
+               mUploads.add(upload);
+             }
+                 ImageAdapter imageAdapter = new ImageAdapter(getApplicationContext(), mUploads);
+                  mRecyclerView.setAdapter(imageAdapter);
+                  imageAdapter.notifyDataSetChanged();
+         }
+
+     }
+
+     @Override
+     public void onCancelled(@NonNull DatabaseError databaseError) {
+
+     }
+ });
+
+
     }
 
 
